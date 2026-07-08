30237b9ed50ab451883f30e98669a67a60c875df
Muhammad Fannan Pratama <muhammadfannan10@gmail.com>
2026-07-08 22:18:19 +0700

    Fix: Add Artisan facade import for migration route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', [FrontendController::class, 'index'])->name('home');
 Route::get('/berita/{id}', [FrontendController::class, 'show'])->name('berita.detail');
+
+Route::get('/jalankan-migrasi', function () {
+    try {
+        Artisan::call('migrate', ['--force' => true]);
+        return 'Sip! Semua tabel database berhasil dibuat di Aiven MySQL.';
+    } catch (\Exception $e) {
+        return 'Gagal migrasi: ' . $e->getMessage();
+    }
+});
